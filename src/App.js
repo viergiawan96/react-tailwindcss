c010db12f351bb9dfa7c7f64b16222f2642dd015
Satrio yudho <47542917+viergiawan96@users.noreply.github.com>
2020-09-08 22:31:54 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/main.css';
+
+function App() {
+  return (
+    <div className="bg-green-500">
+      <h1 className="text-2xl text-white text-center">Tailwind CSS</h1>
+    </div>
+  );
+}
+
+export default App;
